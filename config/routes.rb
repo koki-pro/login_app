b0e23ae3210270776059d4d6a894c0905f931d70
koki-pro <koki.banana03@gmail.com>
2021-02-01 14:20:57 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root 'homes#top'
+  resource :user, only: [:new, :create, :show]
+  get 'login', to: "sessions#new"
+  post 'login', to: "sessions#create"
+  delete 'logout', to: "sessions#destroy"
+end
